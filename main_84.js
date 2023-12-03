b2a0dc30c313522249245d2dc652bd0eb43173c0
lauravp84 <145235659+lauravp84@users.noreply.github.com>
2023-12-03 12:24:06 -0300

    Add files via upload

--- main_84.js
+++ main_84.js
@@ -3,11 +3,11 @@ pizzaListArray = [];
 function getmenu()
 	{    
         // Dê um nome apropriado ao id como pizzaName
-        var item = document.getElementById("pizzaNam").value; 
+        var item = document.getElementById("pizzaName").value; 
        
         //Adicione a função push() para empurrar itens dentro para da array
         pizzaListArray.push(item);
         
-        var removeCommas = pizzaListArray.join("<br># ");
+        var removeCommas = pizzaListArray.join("<br>#");
         document.getElementById("displayMenu").innerHTML =  "# " + removeCommas;
 	}
